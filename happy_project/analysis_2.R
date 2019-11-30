c7d2e504fa0b521fdc12c71403ca6492b53c71fd
nadiadomnina <dna2000@mail.ru>
2019-11-30 14:58:33 -0800

    Update analysis_2.R

--- happy_project/analysis_2.R
+++ happy_project/analysis_2.R
@@ -22,6 +22,10 @@ gov_trust_df <- happy_df %>%
          Trust..Government.Corruption.) %>%
   arrange(desc(Trust..Government.Corruption.))
 
+#r sqaured
+gov_trust.lm <- lm(Happiness.Score ~ Trust..Government.Corruption., data = gov_trust_df)
+gov_trust_r_squared <- summary(gov_trust.lm)$r.squared
+
 
 #GPD DF
 gdp_only_df <- happy_df %>% select(contains("GDP"))
@@ -29,11 +33,19 @@ gdp_happy_df <- happy_df %>%
   select(2:4) %>%
   bind_cols(gdp_only_df)
 
+#r sqaured
+gdp.lm <- lm(Economy..GDP.per.Capita. ~ Happiness.Score, data = gdp_happy_df)
+gdp_r_squared <- summary(gdp.lm)$r.squared
+
 #Health DF
 health_df <- happy_df %>%
   select(Country, Happiness.Score, Happiness.Rank,
          Health..Life.Expectancy.) %>%
   arrange(desc(Health..Life.Expectancy.))
+
+# r squared value
+health.lm <- lm(Health..Life.Expectancy. ~ Happiness.Score, data = health_df)
+health_r_squared <- summary(health.lm)$r.squared
 
 #Employment DF
 employment_df <- happy_df %>%
@@ -48,6 +60,23 @@ employment_df <- happy_df %>%
                   Employment..Services....of.employed., NA),
   )
 employment_df <- na.omit(employment_df)
+
+ # employment r squared value 
+ # 1.
+ employment1.lm = lm(as.numeric(Unemployment....of.labour.force.) ~ Happiness.Score, data=employment_df)
+ employment1_r_squared = summary(employment1.lm)$r.squared
+ # 2.
+ employment2.lm <- lm(as.numeric(Employment..Agriculture....of.employed.) ~ Happiness.Score, data = employment_df)
+ employment2_r_squared <- summary(employment2.lm)$r.squared
+ # 3.
+ employment3.lm <- lm(as.numeric(Employment..Industry....of.employed.) ~ Happiness.Score, data = employment_df)
+ employment3_r_squared <- summary(employment3.lm)$r.squared
+ # 4.
+ employment4.lm <- lm(as.numeric(Employment..Services....of.employed.) ~ Happiness.Score, data = employment_df)
+ employment4_r_squared <- summary(employment4.lm)$r.squared
+ 
+ 
+
 
 
 
