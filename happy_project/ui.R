f5e5b853e6868309c3fd1bb45809b3c800ea7a35
nadiadomnina <dna2000@mail.ru>
2019-12-03 13:41:32 -0800

    seperated r sqaured and analysis for economy

--- happy_project/ui.R
+++ happy_project/ui.R
@@ -25,15 +25,8 @@ main_page <- tabPanel(
   wellPanel(
       p(
           "our to-do list:", br(),
-          "2. write about what the happiness scores are, where they come from, how theyre calculated in the Background Information panel", br(),
-          strong("3. everyone needs to fill out the last about page!!!!!!!"), br(),
-          strong("4. Economy analysis"), br(),
-          "3. Andrey said we should consider adding more interactivity on the maps. For example by displaying only
-      a certain continent at a time or something. I think it would be nice to display the top 10 countries,
-      then those ranked 11-20, 21-30, and so on, you get my point.", br(),
-          "7. maybe add photos of us to the last page if we have the time", br(),
-          "8. make the analysis more reader - friendly.", br(),
-          "9. Add labels to plots. What does .75 life expectancy mean? thaose things"
+          "1. make the analysis more reader - friendly.", br(),
+          strong("2. Add labels to plots AND describe the avriables in the analysis. What does .75 life expectancy mean? those things")
       )
   ),
 
@@ -59,7 +52,7 @@ main_page <- tabPanel(
               "GDP" = 2,
               "Health" = 3,
               "Economy" = 4,
-              "Government Trust" = 5
+              "Government Corruption" = 5
             ),
             selected = 1
           )
@@ -70,6 +63,35 @@ main_page <- tabPanel(
         9,
         plotlyOutput("heat_map")
       )
+    )
+  )
+)
+
+
+research_question_page <- tabPanel(
+  "Information", # label for the tab in the navbar
+  
+  # This content uses a column layouut
+  fluidRow(
+    column(10, 
+           mainPanel(
+             h2("Background Information:"),
+             wellPanel(
+               style = "background: white",
+               p("Happiness scores and rankings are from the Gallup World Poll data over different countries. Happiness scores are based on answer evaluation question such as rate individual’s current lives on scale between 0 to 10. 
+            The scores are nationally representative samples between years 2013 to 2016 in six factors of economic production, social support, life expectancy, freedom, absence of corruption, and generosity. 
+            We also combined this Happiness scores with UN data that shows more characteristics of different countries such as GDP, economy, employment, population growth rate, etc. We used these data to integrate several factors to answer our research question below. 
+            We used coefficient of determination, denoted R squared, to analyze how dependent variable of factors can correlate to the independent variable of Happiness score.", style = "font-size:20px")
+             ),
+             
+             h2("Research Question:"),
+             wellPanel(
+               style = "background: white",
+               p("1. What effect do different country characteristics have on people's happiness?", br(),
+                 "2. Is country happiness calculated based on the same characteristics as other countries,
+            or does each country have a different set of characteristics leading to its happiness?", style = "font-size:20px")
+             )
+           )
     )
   )
 )
@@ -167,20 +189,21 @@ page_three <- tabPanel(
           style = "background: white",
           plotOutput("economy_plot")
         ),
-
         wellPanel(
           style = "background: pink",
-          textOutput("economy_analysis")
+          textOutput("economy_r_squared")
+        ),
+
+        wellPanel(
+          style = "background: white",
+          textOutput("economy_r_sqaured"),
+          textOutput("economy_analysis"),
+          
         )
       )
     )
   )
 )
-
-
-
-
-
 
 
 page_four <- tabPanel(
@@ -203,33 +226,6 @@ page_four <- tabPanel(
           style = "background: white",
           p("In Government trust scatterplot, we found R-squared value of 0.18 with the independent variable of Government trust which have low association with Happiness score. 
                       This represents government trust does not have any correlation with Happiness score.")
-        )
-      )
-    )
-  )
-)
-
-
-research_question_page <- tabPanel(
-  "Information", # label for the tab in the navbar
-
-  # This content uses a column layouut
-  fluidRow(
-    column(10, 
-      mainPanel(
-        h2("Background Information:"),
-        wellPanel(
-          p("Happiness scores and rankings are from the Gallup World Poll data over different countries. Happiness scores are based on answer evaluation question such as rate individual’s current lives on scale between 0 to 10. 
-            The scores are nationally representative samples between years 2013 to 2016 in six factors of economic production, social support, life expectancy, freedom, absence of corruption, and generosity. 
-            We also combined this Happiness scores with UN data that shows more characteristics of different countries such as GDP, economy, employment, population growth rate, etc. We used these data to integrate several factors to answer our research question below. 
-            We used coefficient of determination, denoted R squared, to analyze how dependent variable of factors can correlate to the independent variable of Happiness score.", style = "font-size:20px")
-        ),
-      
-        h2("Research Question:"),
-        wellPanel(
-          p("What effect do different country characteristics have on people's happiness?
-            Is country happiness calculated based on the same characteristics as other countries,
-            or does each country have a different set of characteristics leading to its happiness", style = "font-size:20px")
         )
       )
     )
@@ -283,8 +279,8 @@ about_us_page <- tabPanel(
           h2("Vinay Patel"),
           strong("Graduation Year:"),
           p("2020"),
-          strong("Major/ Intended Major:"),
-          p("Intended Double Major in Informatics and Economics")
+          strong("Intended Major:"),
+          p("Double Major in Informatics and Economics")
         ),
 
         # Hanna
@@ -301,7 +297,7 @@ about_us_page <- tabPanel(
           h2("Vincent Vo"),
           strong("Graduation Year:"),
           p("2022"),
-          strong("Major/ Intended Major:"),
+          strong("Intended Major:"),
           p("Informatics")
         )
       )
